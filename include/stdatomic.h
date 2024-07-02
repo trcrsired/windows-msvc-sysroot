ad6388f46ee921ab1fb84ab3a77b0ada9ef4ed9a
trcrsired <uwgghhbcad@gmail.com>
2024-07-01 20:39:35 -0400

    update stdatomic.h

--- include/stdatomic.h
+++ include/stdatomic.h
@@ -1,7 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 #pragma once
 #ifdef __cplusplus
+#if __has_include(<__msvc_cxx_stdatomic.hpp>)
 #include <__msvc_cxx_stdatomic.hpp>
+#endif
 #else
 #include <vcruntime_c11_stdatomic.h>
 #endif
